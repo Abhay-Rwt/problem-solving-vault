3c893837342d959c0b6b5321580007495b031e42
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2025-11-30 11:23:28 +0530

    Time: 67 ms (18.93%), Space: 54.6 MB (11.68%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -28,8 +28,37 @@ public:
 
     bool isMatch(string s, string p) {
         int n = s.length(), m = p.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1));
 
-        return solve(n, m, s, p, dp);
+        // return solve(n, m, s, p, dp);
+
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        for(int j=0; j<=m; j++){
+            int it = j;
+            while(it > 0 && p[it-1] == '*') it--;
+            dp[0][j] = (it == 0) ? 1 : 0;
+        }
+        dp[0][0] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == p[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    if(p[j-1] == '*'){
+                        dp[i][j] = ( dp[i-1][j-1] || dp[i-1][j] || dp[i][j-1] ); // used, not used, skipped
+                    }
+                    else if(p[j-1] == '?'){
+                        dp[i][j] = dp[i-1][j-1]; // used ?
+                    }
+                    else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
