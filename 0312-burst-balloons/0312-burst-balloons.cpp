93130c5dd4e4ff545386f0b06a46f52f1256fdf7
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2025-12-25 11:20:54 +0530

    Time: 101 ms (64.62%), Space: 13.8 MB (79.06%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -15,7 +15,21 @@ public:
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(1, n-2, nums, dp);
+        vector<vector<int>> dp(n, vector<int>(n));
+        // return solve(1, n-2, nums, dp);
+        for(int i=n-2; i>=1; i--){
+            for(int j=1; j<=n-2; j++){
+                if(i > j){
+                    dp[i][j] = 0;
+                }
+                else{
+                    for(int k=i; k<=j; k++){
+                        int coins = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                        dp[i][j] = max(dp[i][j], coins);
+                    }
+                }
+            }
+        }
+        return dp[1][n-2];
     }
 };
