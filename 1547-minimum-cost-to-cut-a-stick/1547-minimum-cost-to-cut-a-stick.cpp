5dc353d60df43f83b08bc14842f03ecd0d5f28dd
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2026-02-21 12:19:02 +0530

    Time: 38 ms (34.38%), Space: 13.8 MB (77.61%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int ans = INT_MAX;
-        for(int k=i; k<=j; k++){
-            int cost = cuts[j+1] - cuts[i-1] + solve(i, k-1, cuts, dp) + solve(k+1, j, cuts, dp);
-            ans = min(ans, cost);
+    int solve(int low, int high, vector<int> &cuts, vector<vector<int>> &dp){
+        if(low > high) return 0;
+
+        if(dp[low][high] != -1) return dp[low][high];
+
+        int minCost = INT_MAX;
+
+        for(int k=low; k<=high; k++){
+            int cost = cuts[high+1] - cuts[low-1] + solve(low, k-1, cuts, dp) + solve(k+1, high, cuts, dp);
+            minCost = min(minCost, cost);
         }
-        return dp[i][j] = ans;
+
+        return dp[low][high] = minCost;
     }
 
     int minCost(int n, vector<int>& cuts) {
         cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
+
         sort(cuts.begin(), cuts.end());
+
         int size = cuts.size();
-        vector<vector<int>> dp(size, vector<int>(size, 0));
-        // return solve(1, size-2, cuts, dp);
-        for(int i=size-2; i>=1; i--){
-            for(int j=1; j<=size-2; j++){
-                if(i > j){
-                    dp[i][j] = 0;
-                }
-                else{
-                    int ans = INT_MAX;
-                    for(int k=i; k<=j; k++){
-                        int cost = cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j];
-                        ans = min(ans, cost);
-                    }
-                    dp[i][j] = ans;
-                }
-            }
-        }
-        return dp[1][size-2];
+
+        vector<vector<int>> dp(size, vector<int>(size, -1));
+
+        return solve(1, size-2, cuts, dp);    
     }
 };
