59ac470580d5e05c5159575bc4590004d87ea647
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2026-04-04 15:19:43 +0530

    Time: 52 ms (39.48%), Space: 20.7 MB (32.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
     // func(i, j) = no. of ways to achieve j by changing the expressions consedering [0...i] only
-    int dp[21][1001];
+    int dp[21][100001];
 
-    int countWays(int index, int currTarget, vector<int> &nums)
-    {
-        if(index < 0){
-            return 0;
-        }
-        if(index == 0){
-            if(currTarget+nums[0] == 0 && currTarget-nums[0] == 0){
-                return 2;
-            }
-            else if(currTarget+nums[0] == 0 || currTarget-nums[0] == 0){
-                return 1;
-            }
+    // count no. of subsets sum equals to target
+    int solve(int ind, vector<int> &nums, int target){
+        if(ind == 0){
+            if(target == 0 && nums[0] == 0) return 2;
+            if(target == 0 || nums[0] == target) return 1;
             return 0;
         }
 
-        int add = countWays(index-1, currTarget+nums[index], nums);
+        if(dp[ind][target] != -1) return dp[ind][target];
 
-        int sub = countWays(index-1, currTarget-nums[index], nums);
+        int notpick = solve(ind-1, nums, target);
 
-        return add + sub;
+        int pick = 0;
+        if(target >= nums[ind]){
+            pick = solve(ind-1, nums, target - nums[ind]);
+        }
+
+        return dp[ind][target] = notpick + pick;
     }
+    
 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         memset(dp, -1, sizeof(dp));
-        return countWays(n-1, target, nums);
+        // return countWays(n-1, target, nums);
+
+        int totalSum = 0;
+        for(int &it: nums) totalSum += it;
+
+        if(totalSum < abs(target) || (totalSum+target) % 2 != 0){
+            return 0;
+        }
+
+        return solve(n-1, nums, (totalSum+target)/2);
     }
 };
