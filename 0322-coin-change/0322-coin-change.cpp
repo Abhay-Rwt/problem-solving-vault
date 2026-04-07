c895512cbbf919192527dbcda5d926ea71a551ae
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2026-04-07 19:46:12 +0530

    Time: 43 ms (39.73%), Space: 49 MB (34.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,57 @@
 class Solution {
 public:
-    int func(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp){
-        if(amount == 0){
-            return 0;
-        }
-        if(ind < 0){
+    int minCoins(int index, int remAmount, vector<int> &coins, vector<vector<int>> &dp){
+        if(index == 0){
+            if(remAmount%coins[index] == 0){
+                return (remAmount/coins[index]);
+            }
             return 1e8;
         }
-        if(dp[ind][amount] != -1) {
-            return dp[ind][amount];
+
+        if(dp[index][remAmount] != -1){
+            return dp[index][remAmount];
         }
 
-        int np = func(ind-1, amount, coins, dp);
+        int notpick = minCoins(index-1, remAmount, coins, dp);
 
-        int p = 1e8;
-
-        if(coins[ind] <= amount){
-            p = 1 + func(ind, amount-coins[ind], coins, dp);
+        int pick = 1e8;
+        if(remAmount >= coins[index]){ 
+            pick = 1 + minCoins(index, remAmount-coins[index], coins, dp);
         }
 
-        return dp[ind][amount] = min(np, p);
+        return dp[index][remAmount] = min(pick, notpick);
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        vector<vector<int>> dp(n, vector<int>(amount+1));
+        // int result = minCoins(n-1, amount, coins, dp);
+        // return result == 1e8 ? -1 : result;
 
-        int ans = func(n-1, amount, coins, dp);
-        return ans == 1e8 ? -1 : ans;
+        // ############################## TABULATION ######################################
+
+        // for index==0
+        for(int remAmount=0; remAmount<=amount; remAmount++){
+            if(remAmount % coins[0] == 0){
+                dp[0][remAmount] = remAmount / coins[0];
+            }
+            else{
+                dp[0][remAmount] = 1e8;
+            }
+        }
+
+        for(int index=1; index<n; index++){
+            for(int remAmount=0; remAmount<=amount; remAmount++){
+                int notpick = dp[index-1][remAmount];
+                int pick = 1e8;
+                if(remAmount >= coins[index]){
+                    pick = 1 + dp[index][remAmount-coins[index]];
+                }
+
+                dp[index][remAmount] = min(notpick, pick);
+            }
+        }
+
+        return dp[n-1][amount] == 1e8 ? -1 : dp[n-1][amount];
     }
 };
