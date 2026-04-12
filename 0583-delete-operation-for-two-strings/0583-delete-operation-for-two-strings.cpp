3d1f049db4eba97826b7a2bef24e6f65377ea0f3
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2026-04-12 11:19:09 +0530

    Time: 11 ms (66.98%), Space: 16.6 MB (70.51%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(i == 0 && j == 0){
-            if(s[0] == t[0]) return 0;
-            return 2;
-        }
-
-        if(i < 0){
-            int cnt=0;
-            while(j){
-                cnt++;
-                j--;
-            }
-            return cnt+1;
-        }
-
-        if(j < 0){
-            int cnt=0;
-            while(i){
-                cnt++;
-                i--;
-            }
-            return cnt+1;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(s[i] == t[j]){
-            return dp[i][j] = solve(i-1, j-1, s, t, dp);
-        }
-
-        return dp[i][j] = 1 + min(solve(i-1, j, s, t, dp), solve(i, j-1, s, t, dp));
-    }
-
     int minDistance(string word1, string word2) {
-        int n = word1.length(), m = word2.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        // return solve(n-1, m-1, word1, word2, dp);
+        int n = word1.length();
+        int m = word2.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1));
 
-        for(int i=0; i<=n; i++) dp[i][0] = i;
-        for(int j=0; j<=m; j++) dp[0][j] = j;
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=m; j++){
+            dp[0][j] = j;
+        }
+        dp[0][0] = 0;
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
