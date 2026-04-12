2497fba0584851c25bd9bf2c089446b34e2495fe
Abhay Rawat <152484002+Abhay-Rwt@users.noreply.github.com>
2026-04-12 10:57:31 +0530

    Time: 0 ms (100%), Space: 13.1 MB (73.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int solve(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
-        if(i == 0 && j == 0) return 0;
-        if(i > 0 && j == 0) return i;
-        if(i == 0 && j > 0) return j;
+    int minOps(int ind1, int ind2, string &word1, string &word2, vector<vector<int>> &dp)
+    {
+        if(ind1 == 0 && ind2 == 0){
+            return 0;
+        }
+        if(ind2 == 0){
+            return ind1;
+        }
+        if(ind1 == 0){
+            return ind2;
+        }
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
+        }
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if(word1[ind1-1] == word2[ind2-1]){
+            return dp[ind1][ind2] = minOps(ind1-1, ind2-1, word1, word2, dp);
+        }
 
-        if(word1[i-1] == word2[j-1]) return dp[i][j] = solve(i-1, j-1, word1, word2, dp);
+        int replace = 1 + minOps(ind1-1, ind2-1, word1, word2, dp);
+        int del = 1 + min(minOps(ind1-1, ind2, word1, word2, dp), minOps(ind1, ind2-1, word1, word2, dp));
+        int insert = 1 + min(minOps(ind1, ind2-1, word1, word2, dp), minOps(ind1-1, ind2, word1, word2, dp));
 
-        int rep = 1 + solve(i-1, j-1, word1, word2, dp);
-        int del = 1 + min(solve(i-1, j, word1, word2, dp), solve(i, j-1, word1, word2, dp));
-        int ins = 1 + min(solve(i-1, j, word1, word2, dp), solve(i, j-1, word1, word2, dp));
-
-        return dp[i][j] = min(rep, min(del, ins));
+        return dp[ind1][ind2] = min(replace, min(del, insert));
     }
 
     int minDistance(string word1, string word2) {
-        int n = word1.length(), m = word2.length();
+        int n = word1.length();
+        int m = word2.length();
+
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
 
-        return solve(n, m, word1, word2, dp);
+        return minOps(n, m, word1, word2, dp);
     }
 };
